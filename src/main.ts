f51bd73272b2fb3b0bf383425b5bd8a9fcf2a00d
HyunHoo Im <litinglabs03@gmail.com>
2024-03-25 17:50:02 +0900

    main.ts class로 변경

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { Logger } from '@nestjs/common';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+class Application {
+  private readonly logger = new Logger(Application.name);
+  private port: string;
+  private url: string;
+
+  constructor(private server: NestExpressApplication) {
+    this.server = server;
+    this.port = process.env.PORT || '8000';
+  }
+
+  async bootstrap() {
+    await this.server.listen(this.port);
+    this.url = await this.server.getUrl();
+  }
+
+  startLog() {
+    this.logger.log(
+      `Server running on ${this.url.replace(/\[::1\]/, 'localhost')}`,
+    );
+  }
 }
-bootstrap();
+
+async function init() {
+  try {
+    const server = await NestFactory.create<NestExpressApplication>(AppModule);
+    const app = new Application(server);
+    await app.bootstrap();
+
+    app.startLog();
+  } catch (error) {
+    new Logger('init').error(error);
+  }
+}
+
+init();
