12b74a37e27fb49bb2cecf0a30041208d41873ef
HyunHoo Im <litinglabs03@gmail.com>
2024-03-26 17:49:05 +0900

    dto swagger 추가

--- src/cats/dto/cats.dto.ts
+++ src/cats/dto/cats.dto.ts
@@ -1,29 +1,36 @@
-import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateCatDto {
-  @IsNotEmpty()
+  @ApiProperty({
+    description: 'The name of a cat',
+    example: 'Kitty',
+    required: true,
+    type: String,
+  })
   @IsString()
+  @IsNotEmpty()
   name: string;
 
-  @IsNotEmpty()
+  @ApiProperty({
+    description: 'The age of a cat',
+    example: 3,
+    required: true,
+    type: Number,
+  })
   @IsNumber()
+  @IsNotEmpty()
   age: number;
 
-  @IsNotEmpty()
+  @ApiProperty({
+    description: 'The breed of a cat',
+    example: 'Scottish Fold',
+    required: true,
+    type: String,
+  })
   @IsString()
+  @IsNotEmpty()
   breed: string;
 }
 
-export class UpdateCatDto {
-  @IsOptional()
-  @IsString()
-  name: string;
-
-  @IsOptional()
-  @IsNumber()
-  age: number;
-
-  @IsOptional()
-  @IsString()
-  breed: string;
-}
+export class UpdateCatDto extends PartialType(CreateCatDto) {}
