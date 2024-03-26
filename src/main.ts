cdb0ccf8cf75ab41ab24a237bf54c136c5fb3721
HyunHoo Im <litinglabs03@gmail.com>
2024-03-26 12:24:52 +0900

    session 추가

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { Logger } from '@nestjs/common';
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
 import helmet, { contentSecurityPolicy } from 'helmet';
+import session from 'express-session';
 
 dotenv.config();
 
@@ -19,9 +20,13 @@ class Application {
     'default-src': [],
     'script-src': [],
   };
+  private sessionSecret: string;
 
   constructor(private server: NestExpressApplication) {
     this.server = server;
+
+    !process.env.SESSION_SECRET &&
+      this.logger.error('session secret 설정이 필요합니다!'); // 세션 비밀 설정이 없는 경우 에러 출력
     this.port = process.env.PORT || '8000';
     this.corsOrigin = process.env.CORS_ORIGIN
       ? process.env.CORS_ORIGIN.split(',').map((origin) => origin.trim())
@@ -32,6 +37,7 @@ class Application {
     process.env.SCRIPT_SRC?.split(',').forEach((src) =>
       this.directives['script-src'].push(src.trim()),
     );
+    this.sessionSecret = process.env.SESSION_SECRET || 'secret';
   }
 
   policy() {
@@ -52,8 +58,25 @@ class Application {
     });
   }
 
+  session() {
+    this.server.use(
+      session({
+        secret: Buffer.from(this.sessionSecret).toString('base64'), // 세션을 안전하게 유지하기 위한 비밀
+        resave: false, // 세션에 변경사항이 없으면 다시 저장하지 않음
+        saveUninitialized: false, // 초기화되지 않은 세션을 스토어에 저장하지 않음
+        cookie: {
+          secure: process.env.ENV === 'production', // https 프로토콜을 사용하는 경우 true
+          httpOnly: true, // 클라이언트에서 쿠키를 확인하지 못하도록 함
+          maxAge: 1000 * 60 * 60, // 쿠키 유효 시간
+          sameSite: 'none', // 쿠키 전송 위치 설정
+        },
+      }),
+    );
+  }
+
   async bootstrap() {
     this.policy();
+    this.session();
     await this.server.listen(this.port);
     this.url = await this.server.getUrl();
   }
