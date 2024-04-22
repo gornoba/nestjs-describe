00d6c60da25b7e02898e7c1f4af66fae132fc02c
HyunHoo Im <litinglabs03@gmail.com>
2024-04-22 17:03:21 +0900

    추가수정

--- src/mongoDb/mongo.module.ts
+++ src/mongoDb/mongo.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { MongoController } from './mongo.controller';
 import { MongoService } from './mongo.service';
 import { PersonMongoRepository } from './repositories/persion.mongo.repository';
-import { PersonMongoEntity } from './entities/person.mongo.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PersonMongoEntity], 'mongo')],
+  imports: [],
   controllers: [MongoController],
   providers: [MongoService, PersonMongoRepository],
   exports: [],
